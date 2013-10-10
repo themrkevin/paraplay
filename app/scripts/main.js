50e7a2a5ec54d202bb79147744000f99854390fd
Kevin Mangubat <kevin.mangu@gmail.com>
2013-10-10 15:41:34 -0400

    doge attacks

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,5 +1,7 @@
 window.onscroll = function() {
 	paraplay('.background','top',0.2);
+	// unicornTest();
+	doge();
 }
 
 function paraplay(el,direction,speed) {
@@ -8,8 +10,28 @@ function paraplay(el,direction,speed) {
 	element.style[direction] = -(scrolled*speed)+'px';
 }
 
-function unicornTest() {
-	var scrolled = window.pageYOffset,
-		unicorn = document.querySelector('.unicorn');
-	// element.style.l
+function doge() {
+	var dogeSec = document.getElementById('doge'),
+		scrolled = window.pageYOffset,
+		dogeTop = dogeSec.offsetTop,
+		dogeSpy = document.querySelector('.dogespy'),
+		doge1 = document.querySelector('.doge1'),
+		dogeR = document.querySelector('.doger'),
+		dogeB = document.querySelector('.dogeb');
+
+	var dogeBClasses = dogeB.className;
+
+	dogeSpy.style.bottom = (-(scrolled) - 200) + 'px';
+
+	if(dogeTop - scrolled > 0) {
+		doge1Formula = (dogeTop - scrolled)*0.8;
+		doge1.style.top = doge1Formula + 'px';
+		dogeR.style.top = doge1.height + 10 + (dogeTop - scrolled) + 'px';
+
+		if(doge1Formula > 60) {
+			console.log('yes',doge1.style.top);
+		} else {
+			console.log('no',doge1.style.top);
+		}
+	}
 }
