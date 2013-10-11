4c43516179e198d3d32e9fcbbc39876c662ae38d
Kevin Mangubat <kevin.mangu@gmail.com>
2013-10-11 01:44:05 -0400

    adding normalize css and some helper functions to add and remove classes

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,26 +1,20 @@
-window.onload = function() {
-	classy(document.body);
-}
 window.onscroll = function() {
 	paraplay('.background','top',0.2);
-	// unicornTest();
 	doge();
 }
-
 function paraplay(el,direction,speed) {
 	var scrolled = window.pageYOffset,
 		element = document.querySelector(el);
 	element.style[direction] = -(scrolled*speed)+'px';
 }
-
 function doge() {
 	var dogeSec = document.getElementById('doge'),
 		scrolled = window.pageYOffset,
 		dogeTop = dogeSec.offsetTop,
-		dogeSpy = document.querySelector('.dogespy'),
-		doge1 = document.querySelector('.doge1'),
-		dogeR = document.querySelector('.doger'),
-		dogeB = document.querySelector('.dogeb');
+		dogeSpy = dogeSec.querySelector('.dogespy'),
+		doge1 = dogeSec.querySelector('.doge1'),
+		dogeR = dogeSec.querySelector('.doger'),
+		dogeB = dogeSec.querySelector('.dogeb');
 
 	dogeSpy.style.bottom = (-(scrolled) - 200) + 'px';
 
@@ -29,17 +23,40 @@ function doge() {
 		doge1.style.top = doge1Formula + 'px';
 		dogeR.style.top = doge1.height + 10 + (dogeTop - scrolled) + 'px';
 
-		if(doge1Formula > 60) {
-			console.log('yes',doge1.style.top);
+		if(doge1Formula < 80) {
+			addClass(dogeB,'ohhai');
+			// console.log('yes',doge1.style.top);
 		} else {
-			console.log('no',doge1.style.top);
+			removeClass(dogeB,'ohhai');
+			// console.log('no',doge1.style.top);
 		}
 	}
 }
-
-function classy(el,action) {
+function addClass(el,name) {
 	var classes = el.className.split(' ');
-	console.log(classes);
-	var classify = classes.join(' ');
-	console.log(classify);
+	if(classes.inArray(name)) return false;
+	classes.push(name);
+	classes = classes.join(' ');
+	el.className = classes;
+}
+function removeClass(el,name) {
+	var classes = el.className.split(' '),
+		l = classes.length;
+	if(!classes.inArray(name)) return false;
+	classes.remove(classes.indexOf(name))
+	el.className = classes;
+}
+// Check if something is inside an array
+Array.prototype.inArray = function(name) {
+	var l = this.length;
+	for(i=0; i<l; i++) {
+		if(this[i] === name) return true;
+	}
+	return false;
+}
+// Array Remove - By John Resig (MIT Licensed)
+Array.prototype.remove = function(from, to) {
+  var rest = this.slice((to || from) + 1 || this.length);
+  this.length = from < 0 ? this.length + from : from;
+  return this.push.apply(this, rest);
 }
